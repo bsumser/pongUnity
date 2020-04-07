1c0a7e2b54a10b41dcdb1a62f4e7acb2e6d0fb3d
gingikid <bsumser@gmail.com>
2020-04-06 20:39:01 -0700

    first commit and finished

---
+++ Assets/paddle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+    public bool isPlayer1; //distinguishes between player 1 and 2
+    public float speed; //speed of player movement
+    public Rigidbody2D rb; //reference to the rigid body
+    public Vector3 startPosition;
+
+    public float movement; //determine if moving up or down
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else 
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        } 
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+    }
+}
